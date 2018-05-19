f1f6cf7d1571b4cd3f50f89c6e4b845fcde392ca
Silwer0k <cupomaaax@gmail.com>
2018-05-19 23:23:17 +0300

    Возможность распредленения характеристик перса, их синхронизация

---
+++ TestingTest/Assets/Scripts/HeroCharacteristics.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HeroCharacteristics : NetworkBehaviour {
+
+    [SyncVar]
+    public int agility;
+    [SyncVar]
+    public int power;
+    [SyncVar]
+    public int inteintelligence;
+
+    [SyncVar]
+    private double damage;
+    [SyncVar]
+    private int health;
+    [SyncVar]
+    private float evasion;
+
+    public void SetHealth(int new_health)
+    {
+        health = new_health;
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public void SetDamage(double new_damage)
+    {
+        damage = new_damage;
+    }
+
+    public double GetDamage()
+    {
+        return damage;
+    }
+
+    public void SetEvasion(float new_evasion)
+    {
+        evasion = new_evasion;
+    }
+
+    public float GetEvasion()
+    {
+        return evasion;
+    }
+
+    public double CalcDamage(int i)
+    {
+        return 20 * i * 0.25;
+    }
+
+    public int CalcHealth(int p)
+    {
+        int h;
+        if (p <= 5)
+            h = p * 20;
+        else
+            h = p * 15;
+
+        return h;
+    }
+
+    public float CalcEvasion(int a)
+    {
+        switch (a)
+        {
+            case 1:
+                return 0;
+            case 2:
+                return 0.1f;
+            case 3:
+                return 0.1f;
+            case 4:
+                return 0.3f;
+            case 5:
+                return 0.3f;
+            case 6:
+                return 0.45f;
+            case 7:
+                return 0.5f;
+            case 8:
+                return 0.6f;
+            case 9:
+                return 0.7f;
+            case 10:
+                return 0.75f;
+            default:
+                return 0.8f;
+        }
+    }
+
+    public void SetCharacteristics(int p, int a, int i, int h, double d, float e)
+    {
+        power = p;
+        agility = a;
+        inteintelligence = i;
+        health = h;
+        damage = d;
+        evasion = e;
+    }
+}
--- TestingTest/Assets/Scripts/HeroChoice.cs
+++ TestingTest/Assets/Scripts/HeroChoice.cs
@@ -7,28 +7,79 @@ using TMPro;
 
 public class HeroChoice : MonoBehaviour {
 
-	public Sprite[] heroesSprites;
+	public GameObject[] heroes;
 	public string[] heroesNames;
 	private short curnum = 0;
 
-	void Start()
-	{
-		ChangeSprite (curnum);
-	}
+    TMP_Text powerCntrTxt;
+    TMP_Text agilityCntrTxt;
+    TMP_Text intelligenceCntrTxt;
+    TMP_Text healthCntrTxt;
+    TMP_Text damageCntrTxt;
+    TMP_Text evasionCntrTxt;
+    TMP_Text freePointsCntrTxt;
 
-	void ChangeSprite(int num)
+    int powerCntr;
+    int agilityCntr;
+    int intelligenceCntr;
+    int healthCntrerCntr;
+    double damageCntr;
+    float evasionCntr;
+
+    int freePoints;
+
+    void Start()
 	{
-		gameObject.GetComponentsInChildren<Image>()[1].sprite = heroesSprites [num];
-		gameObject.GetComponentInChildren<TMP_Text> ().text = heroesNames [num]; 
+        powerCntrTxt = GameObject.Find("PowerCntrTxt").GetComponent<TMP_Text>();
+        agilityCntrTxt = GameObject.Find("AgilityCntrTxt").GetComponent<TMP_Text>();
+        intelligenceCntrTxt = GameObject.Find("intelligenceCntrTxt").GetComponent<TMP_Text>();
+        healthCntrTxt = GameObject.Find("HealthCntrTxt").GetComponent<TMP_Text>();
+        damageCntrTxt = GameObject.Find("DamageCntrTxt").GetComponent<TMP_Text>();
+        evasionCntrTxt = GameObject.Find("EvasionCntrTxt").GetComponent<TMP_Text>();
+        freePointsCntrTxt = GameObject.Find("FreePointsCntrTxt").GetComponent<TMP_Text>();
+
+        ChangeHero(curnum);
+    }
+
+    void SetupUIComponents()
+    {
+        powerCntrTxt.text = powerCntr.ToString();
+        agilityCntrTxt.text = agilityCntr.ToString();
+        intelligenceCntrTxt.text = intelligenceCntr.ToString();
+        healthCntrTxt.text = healthCntrerCntr.ToString();
+        damageCntrTxt.text = damageCntr.ToString();
+        evasionCntrTxt.text = (evasionCntr*100).ToString()+"%";
+        freePointsCntrTxt.text = freePoints.ToString();
+    }
+
+    void SetupMainCharacteristics(int p, int a, int i)
+    {
+        powerCntr = p;
+        agilityCntr = a;
+        intelligenceCntr = i;
+    }
+
+    void ChangeHero(int num)
+	{
+		gameObject.GetComponentsInChildren<Image>()[1].sprite = heroes [num].GetComponent<SpriteRenderer>().sprite;
+		gameObject.GetComponentInChildren<TMP_Text> ().text = heroesNames [num];
+
+        HeroCharacteristics heroChars = heroes[num].GetComponent<HeroCharacteristics>();
+        SetupMainCharacteristics(heroChars.power, heroChars.agility, heroChars.inteintelligence);
+        damageCntr = heroChars.CalcDamage(intelligenceCntr);
+        healthCntrerCntr = heroChars.CalcHealth(powerCntr);
+        evasionCntr = heroChars.CalcEvasion(agilityCntr);
+        freePoints = 0;
+        SetupUIComponents();
 	}
 
 	public void NextHero()
 	{
-		if (curnum < (heroesSprites.Length - 1)) 
+		if (curnum < (heroes.Length - 1)) 
 		{
 			curnum += 1;
-		} 
-		ChangeSprite (curnum);
+		}
+        ChangeHero(curnum);
 	}
 
 	public void PrevHero()
@@ -36,14 +87,93 @@ public class HeroChoice : MonoBehaviour {
 		if (curnum != 0) 
 		{
 			curnum -= 1;
-			ChangeSprite (curnum);
 		}
-		ChangeSprite (curnum);
+        ChangeHero(curnum);
 	}
 
-	public void AcceptChoise()
-    { 
-        ClientScene.AddPlayer (curnum);
-	}
+    public void AcceptChoise()
+    {
+        var prefs = NetworkManagerCustom.singleton.spawnPrefabs;
+        prefs[curnum + 1].GetComponent<HeroCharacteristics>().SetCharacteristics(powerCntr,agilityCntr,intelligenceCntr,healthCntrerCntr,damageCntr,evasionCntr);
+        ClientScene.AddPlayer(curnum);
+    }
+
+    public void PowerUp()
+    {
+        if (freePoints > 0)
+        {
+            freePoints -= 1;
+            powerCntr += 1;
+            HeroCharacteristics heroChars = heroes[curnum].GetComponent<HeroCharacteristics>();
+            healthCntrerCntr = heroChars.CalcHealth(powerCntr);
+            SetupMainCharacteristics(powerCntr, agilityCntr, intelligenceCntr);
+            SetupUIComponents();
+        }
+    }
+
+    public void PowerDown()
+    {
+        if (powerCntr > 1)
+        {
+            freePoints += 1;
+            powerCntr -= 1;
+            HeroCharacteristics heroChars = heroes[curnum].GetComponent<HeroCharacteristics>();
+            healthCntrerCntr = heroChars.CalcHealth(powerCntr);
+            SetupMainCharacteristics(powerCntr, agilityCntr, intelligenceCntr);
+            SetupUIComponents();
+        }
+    }
+
+    public void AgilityUp()
+    {
+        if (freePoints > 0)
+        {
+            freePoints -= 1;
+            agilityCntr += 1;
+            HeroCharacteristics heroChars = heroes[curnum].GetComponent<HeroCharacteristics>();
+            evasionCntr = heroChars.CalcEvasion(agilityCntr);
+            SetupMainCharacteristics(powerCntr, agilityCntr, intelligenceCntr);
+            SetupUIComponents();
+        }
+    }
+
+    public void AgilityDown()
+    {
+        if (agilityCntr > 1)
+        {
+            freePoints += 1;
+            agilityCntr -= 1;
+            HeroCharacteristics heroChars = heroes[curnum].GetComponent<HeroCharacteristics>();
+            evasionCntr = heroChars.CalcEvasion(agilityCntr);
+            SetupMainCharacteristics(powerCntr, agilityCntr, intelligenceCntr);
+            SetupUIComponents();
+        }
+    }
+
+    public void intelligenceUp()
+    {
+        if (freePoints > 0)
+        {
+            freePoints -= 1;
+            intelligenceCntr += 1;
+            HeroCharacteristics heroChars = heroes[curnum].GetComponent<HeroCharacteristics>();
+            damageCntr = heroChars.CalcDamage(intelligenceCntr);
+            SetupMainCharacteristics(powerCntr, agilityCntr, intelligenceCntr);
+            SetupUIComponents();
+        }
+    }
+
+    public void intelligenceDown()
+    {
+        if (intelligenceCntr > 1)
+        {
+            freePoints += 1;
+            intelligenceCntr -= 1;
+            HeroCharacteristics heroChars = heroes[curnum].GetComponent<HeroCharacteristics>();
+            damageCntr = heroChars.CalcDamage(intelligenceCntr);
+            SetupMainCharacteristics(powerCntr, agilityCntr, intelligenceCntr);
+            SetupUIComponents();
+        }
+    }
 
 }
